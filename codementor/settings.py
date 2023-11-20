cf7beb7baf2db102b98d87b9bb81bd7f98086fba
Andidiong <usoroandidiong@gmail.com>
2023-11-20 20:13:57 +0100

    first commit

--- codementor/settings.py
+++ codementor/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'codementor.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation

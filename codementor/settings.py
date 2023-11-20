03aa93d3388b01b0b9373cf7fa5e1cbc646109ed
Andidiong <usoroandidiong@gmail.com>
2023-11-20 20:11:49 +0100

    first commit

--- codementor/settings.py
+++ codementor/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'codementor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'e16dGD4e4GC23gE2Daa143AdfC*aG*A-',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '58688',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'e16dGD4e4GC23gE2Daa143AdfC*aG*A-',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '58688',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
